f6653817136fdd290735392bd957078dd4fbbfe9
unknown <rafaelgasparoto4@gmail.com>
2025-05-14 00:03:04 -0300

    Rotas
    /carta
    /pedido
    /usuario

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CartaController;
+use App\Http\Controllers\PedidoController;
+use App\Http\Controllers\UsuarioController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::apiResource('carta', CartaController::class);
+
+Route::apiResource('pedido', PedidoController::class);
+
+Route::apiResource('usuario', UsuarioController::class);
