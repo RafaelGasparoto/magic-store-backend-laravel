a1e71a71139d2bb50a57663129bd36ce5d9eb9fa
unknown <rafaelgasparoto4@gmail.com>
2025-05-14 22:24:45 -0300

    Ajuste rotas para API

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CartaController;
-use App\Http\Controllers\PedidoController;
-use App\Http\Controllers\UsuarioController;
-
-Route::apiResource('carta', CartaController::class);
-
-Route::apiResource('pedido', PedidoController::class);
-
-Route::apiResource('usuario', UsuarioController::class);
